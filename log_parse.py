1f250bbe808806a568f09d19d125e3a615001b1a
Junya Morioka <mjun@mjunya.com>
2022-03-13 18:48:32 +0900

    update tabulate

--- log_parse.py
+++ log_parse.py
@@ -1,78 +1,71 @@
 import pathlib
-import re
 import argparse
-import pandas as pd
+
+from tabulate import tabulate
 
 SEARCH_TEXT = ["Accuracy", "Speed"]
 
 
-def atoi(text):
-    return int(text) if text.isdigit() else text
+def parse_log(base_dir, format="simple"):
+    test_dirs = pathlib.Path(base_dir).glob("**/runs/?*/")
+    table_data = []
+    parse_error = []
 
-
-def natural_keys(text):
-    return [atoi(c) for c in re.split(r"(\d+)", text)]
-
-
-def parse_log(result_dir):
-    directories = pathlib.Path(result_dir).glob("**/runs/?*/")
-    names = []
-    results = []
-    non_exist = []
-    for d in directories:
+    for d in test_dirs:
         if not d.is_dir():
             continue
-        name = [d.parents[1].name, d.name]
-        log = d / "test.log"
-        try:
-            with open(log, "r") as f:
-                log_txt = f.readlines()
-            result = []
-            for text in SEARCH_TEXT:
-                for row in reversed(log_txt):
-                    if text in row:
-                        # if text ==
-                        if text == "Speed":
-                            row = row.split(",")[-1]
-                        result.append(row)
-                        break
 
-            result = list(map(lambda x: x.split(":")[-1].strip(), result))
-            if len(result) == 0:
-                raise ValueError
-            row = [*name, *result]
-            names.append("/".join(name))
-            results.append(row)
+        data = {}
+        data["name"] = d.parents[1].name
+        data["runs"] = d.name
+
+        try:
+            with open(d / "test.log", "r") as f:
+                log_txt = f.readlines()
+            for keyword in SEARCH_TEXT:
+                for row in reversed(log_txt):
+                    if keyword in row:
+                        # [2021-11-26 07:51:17,843][INFO] AP@IoU=0.75: 0.99500 -> AP@IoU=0.75: 0.99500
+                        metrics = row.strip().split("]")[-1]
+                        # AP@IoU=0.75: 0.99500 -> 0.99500
+                        metrics = metrics.split(":")[-1].strip()
+
+                        ####### If you use specific log, edit code below. #######
+                        # if keyword == "Speed":
+                        #     # Average Inferance Speed: 0.02623s, 38.12fps
+
+                        data[keyword] = metrics
+                        break
+            if len(data.keys()) > 2:
+                table_data.append(data)
+            else:
+                raise KeyError
         except Exception:
-            non_exist.append("/".join(name))
+            parse_error.append(data["name"])
             continue
 
-    pd.options.display.colheader_justify = "left"
-    pd.set_option("display.width", None)
-    df = (
-        pd.DataFrame(data=results, columns=["NAME", "RUNS", *SEARCH_TEXT])
-        .sort_values(["NAME", "RUNS"])
-        .reset_index(drop=True)
-    )
-    max_name = len(max(df["NAME"], key=len))
-    max_runs = len(max(df["RUNS"], key=len))
+    print(tabulate(table_data, headers="keys", tablefmt=format))
 
-    df["NAME"] = df["NAME"].map(f"{{:<{max_name}}}".format)
-    df["RUNS"] = df["RUNS"].map(f"{{:<{max_runs}}}".format)
-    print(df)
-    if len(non_exist) > 0:
-        print("non_exist:", non_exist)
+    if len(parse_error) > 0:
+        print("parse_error:", parse_error)
 
 
 def arg_parse():
     parser = argparse.ArgumentParser(description="parse test_log.txt")
     parser.add_argument("result", help="result_dir")
+    parser.add_argument(
+        "--format",
+        help="table format. Default value is 'simple'",
+        type=str,
+        default="simple",
+        choices=["simple", "plain", "html", "latex", "latex_row", "github"],
+    )
     return parser.parse_args()
 
 
 def main():
     args = arg_parse()
-    parse_log(args.result)
+    parse_log(args.result, format=args.format)
 
 
 if __name__ == "__main__":
