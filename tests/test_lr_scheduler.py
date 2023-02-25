2e5d43f2c32653c476993294791237ad4b70b2ba
Junya Morioka <mjun@mjunya.com>
2023-02-25 11:55:08 +0900

    update

--- tests/test_lr_scheduler.py
+++ tests/test_lr_scheduler.py
@@ -1,20 +1,30 @@
 import torch.optim as optim
 import torch.nn as nn
 import matplotlib.pyplot as plt
+from timm.scheduler import CosineLRScheduler
 
 
 def main():
     model = nn.Sequential(nn.Conv2d(3, 32, 3))
     optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=0.1)
-    lr_scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
-        optimizer, T_0=95, T_mult=5, eta_min=1e-5
+    # lr_scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
+    #     optimizer, T_0=95, T_mult=5, eta_min=1e-5
+    # )
+    lr_scheduler = CosineLRScheduler(
+        optimizer,
+        t_initial=100,
+        lr_min=1e-5,
+        warmup_t=5,
+        warmup_lr_init=1e-4,
+        cycle_limit=1,
+        warmup_prefix=True,
     )
 
     epochs = list(range(100))
     lrs = []
-    for _ in epochs:
-        lrs.append(lr_scheduler.get_last_lr()[0])
-        lr_scheduler.step()
+    for epoch in epochs:
+        lrs.append(optimizer.param_groups[0]["lr"])
+        lr_scheduler.step(epoch=epoch)
 
     draw(epochs, lrs, "")
 
