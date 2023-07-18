73be852cf312c1f737cb2e82e56b86cc67c9e667
Junya Morioka <mjun@mjunya.com>
2023-07-18 12:59:10 +0900

    update

--- src/models/model.py
+++ src/models/model.py
@@ -4,13 +4,11 @@ from ..losses import build_loss
 from .build import MODEL_REGISTRY
 
 
-@MODEL_REGISTRY.register()
+# @MODEL_REGISTRY.register()
 class BaseModel(nn.Module):
     def __init__(self, cfg, phase):
         self.cfg = cfg
         self.phase = phase
-
-        self.criterion = build_loss(cfg)
 
     def forward(self, x, data=None):
         return x
