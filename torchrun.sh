66f2ec43095714a58c4592d4c5aa31ab78440d59
Junya Morioka <mjun@mjunya.com>
2023-08-01 01:59:30 +0900

    update

--- torchrun.sh
+++ torchrun.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 NUM_GPU=$1
 shift
-OMP_NUM_THREADS=${NUM_GPU} torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1 --nproc_per_node=${NUM_GPU} "$@" GPU.MULTI=True
+OMP_NUM_THREADS=$(expr 3 \* $NUM_GPU) torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1 --nproc_per_node=${NUM_GPU} "$@" GPU.MULTI=True
