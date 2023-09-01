31f5a42067be4677d0b81e4a756bac35b3957168
Junya Morioka <mjun@cs.meiji.ac.jp>
2023-09-02 00:48:18 +0900

    update

--- src/trainer.py
+++ src/trainer.py
@@ -88,7 +88,7 @@ class Trainer:
                     self.scaler.update()
                     if self.iter_lr_scheduler:
                         self.iter_lr_scheduler.step(epoch=i, metric=loss["total_loss"].item())
-                    self.optimizer.zero_grad()
+                    self.optimizer.zero_grad(set_to_none=True)
                     if model_ema:
                         model_ema.update(model)
 
