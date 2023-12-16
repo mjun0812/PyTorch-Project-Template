d9b8a69620c909e21673c16c920484da739bcf59
Junya Morioka <mjun@mjunya.com>
2023-12-16 18:08:43 +0900

    update

--- src/models/model.py
+++ src/models/model.py
@@ -1,14 +1,16 @@
 import torch.nn as nn
 
+from .backbone import build_backbone
 from .build import MODEL_REGISTRY  # noqa
 
 
 @MODEL_REGISTRY.register()
 class BaseModel(nn.Module):
-    def __init__(self, cfg, phase):
+    def __init__(self, cfg, phase="train"):
         super().__init__()
         self.cfg = cfg
         self.phase = phase
+        self.backbone, _ = build_backbone(self.cfg)
 
     def forward(self, x, data=None):
-        return x
+        return self.backbone(x)[0]
