2298c7239568b376313bcdfa715c04f84c510a60
Junya Morioka <mjun@mjunya.com>
2024-10-24 00:46:09 +0900

    update

--- src/dataloaders/tensor_cache.py
+++ src/dataloaders/tensor_cache.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 import torch
-from torch.multiprocessing import Lock, Manager, Value
+from torch.multiprocessing import Manager, Value
 
 BYTES_PER_GIB = 1024**3
 
@@ -12,38 +12,35 @@ class TensorCache:
         self.manager = Manager()
         self.cache = self.manager.dict()
         self.current_cache_size = Value("L", 0)  # 符号なしlong型
-        self.cache_lock = Lock()
-        self.max_cache_size = size_limit_gb
+        self.is_full = False
 
     def __contains__(self, key: str):
-        with self.cache_lock:
-            return key in self.cache
+        return key in self.cache
 
     def _calc_elm_size(self, elm: torch.Tensor) -> int:
         size = elm.numel() * elm.dtype.itemsize
         return size
 
     def set(self, key: str, value: torch.Tensor) -> bool:
+        if self.is_full:
+            return False
         size = self._calc_elm_size(value)
-        with self.cache_lock:
-            if self.current_cache_size.value + size > self.max_cache_size:
-                return False
-            self.cache[key] = value
-            self.current_cache_size.value += size
+        if self.current_cache_size.value + size > self.size_limit:
+            self.is_full = True
+            return False
+        self.cache[key] = value
+        self.current_cache_size.value += size
         return True
 
     def get(self, key: str) -> Optional[torch.Tensor]:
-        with self.cache_lock:
-            return self.cache.get(key, None)
+        return self.cache.get(key, None)
 
     def clear(self):
-        with self.cache_lock:
-            self.cache.clear()
-            self.current_cache_size.value = 0
+        self.cache.clear()
+        self.current_cache_size.value = 0
 
     def remove(self, key: str):
-        with self.cache_lock:
-            if key in self.cache:
-                size = self._calc_elm_size(self.cache[key])
-                del self.cache[key]
-                self.current_cache_size.value -= size
+        if key in self.cache:
+            size = self._calc_elm_size(self.cache[key])
+            del self.cache[key]
+            self.current_cache_size.value -= size
